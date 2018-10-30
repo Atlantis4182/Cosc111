1e8724e2167a9ec8a5e531fbed310a5e03d1a213
Alex Rogov <alexandre.rogov@gmail.com>
2018-10-29 17:51:46 -0700

    Q3 of A6

--- Assignment#6/src/assignment6/Q3.java
+++ Assignment#6/src/assignment6/Q3.java
@@ -6,27 +6,39 @@ public class Q3 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 	
-		System.out.println("Enter a positive integer: ");
+		System.out.print("Enter a positive integer: ");
 		int num = in.nextInt();
 		
-		
+		if (isPalindrome(num) == true)
+		System.out.printf("%d is a palindrome",num);
+		else
+		System.out.printf("%d is not a palindrome",num);
 		in.close();
 	}
 	
 	public static int reverse(int number) {
 		
-		for (int i = 0; i < ; i++) {
+		String numStringF = String.valueOf(number);  //changing int to String		
+		int numStringF2 = numStringF.length() - 1;
+		int reverse = 0;
+		int reverse2 = 0;
+		
+		for (int i = numStringF2; i >= 0 ; i--) {
+			int numR = numStringF.charAt(i) - 48;    //numR -> "number reversed" //4, i =4
+				reverse = numR * (int)(Math.pow(10, i)); // eg.num=3456 -> 6000+500+40+3=6543
+				reverse2 += reverse; 
 			
 		}
-		
-		
-		return reverse;
+		return reverse2;
 	}
 	
 	public static boolean isPalindrome(int number) {
 		
-		
-		
-		
+		boolean status = false;
+				
+		if (number == reverse(number))
+			status = true;
+
+		return status;
 	}
 }
---
+++ cosc111_lecture/src/lec14/Ex1.java
@@ -0,0 +1,9 @@
+package lec14;
+
+public class Ex1 {
+	public static void main(String[] args) {
+	
+		
+		System.out.println(1 * (int)(Math.pow(10,4 )));
+}
+}
